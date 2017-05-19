345e2919364dd7e69dc9620bf0bf6890609c082a
Björn Þór Jónsson <bangsi@bthj.is>
2017-05-19 10:05:23 +0000

    minor update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 10.2. Pod IP: '; ?>
+ <?php echo 'App Version: 10.2000000000. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
